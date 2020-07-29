7dfe6eafb9cdfa4572e928e0949d224d7cdb1205
friedrau <67593537+friedrau@users.noreply.github.com>
2020-07-28 17:44:00 -0700

    Update bst.c

--- bst.c
+++ bst.c
@@ -98,7 +98,24 @@ struct Node* BSTnodeAdd(struct Node* current, TYPE value) {
  
 
 int containsBSTree(BSTree* myTree, TYPE value) {
+	assert(myTree != 0); //Check if tree is null
+	assert(value != 0); // Check if value is null
+	struct Node* iterator = myTree->root; // type cast
 
+
+	while (iterator != 0) {  //While Iterator is not null
+
+		if (compare(value, iterator->value) == 0) { //Compare the value to current iterator value  and see if == 0 (Same)
+			return 1;
+		}
+		else if (compare(value, iterator->value) < 0) { //Compare the value to current iterator value  and see if == 1 (less then)
+			iterator = iterator->left;
+		}
+		else {
+			iterator = iterator->right;
+		}
+	}
+	return 0;
 }
 
 TYPE minBSTree(BSTree* myTree) {
@@ -108,10 +125,53 @@ TYPE minBSTree(BSTree* myTree) {
 TYPE maxBSTree(BSTree* myTree) {
 
 }
+struct Node* _removeNode(struct Node* cur, TYPE val)
+{
+	/*write this*/
+	assert(cur != 0);
+	assert(val != NULL);
 
-void removeBSTree(BSTree *tree, TYPE value) {
-   
-}  
+	struct Node* temp;
+
+	if (compare(cur->val, val) == 0) {
+		if (cur->right == 0) {
+			temp = cur->left;
+			free(cur);
+			return temp;
+		}
+		else {
+			cur->val = _leftMost(cur->right);
+			cur->right = _removeLeftMost(cur->right);
+			return cur;
+		}
+
+	}
+	else if (compare(val, cur->val) == 1) {
+		cur->right = _removeNode(cur->right, val);
+		return cur;
+
+	}
+	else {
+		cur->left = _removeNode(cur->left, val);
+		return cur;
+	}
+}
+/*
+ function to remove a value from the binary search tree
+ param: tree   the binary search tree
+		val		the value to be removed from the tree
+ pre:	tree is not null
+		val is not null
+		val is in the tree
+ pose:	tree size is reduced by 1
+ */
+void removeBSTree(BSTree* tree, TYPE value)
+{
+	if (containsBSTree(tree, value)) {
+		tree->root = _removeNode(tree->root, value);
+		tree->count--;
+	}
+}
 
 void printBSTree(BSTree* myTree) {
 
